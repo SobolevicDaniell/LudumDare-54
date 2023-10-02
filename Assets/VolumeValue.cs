29fd070c558834a0c4fb8b2e5f406aab016aa562
HOUNd212 <kristalik767@gmail.com>
2023-10-02 19:56:23 +0300

    FixMenu

---
+++ Assets/VolumeValue.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValue : MonoBehaviour
+{
+   private AudioSource audioScr;
+   private float musicVolume = 1f;
+
+   void Start()
+   {
+      audioScr = GetComponent<AudioSource>();
+   }
+
+   void Update()
+   {
+      audioScr.volume = musicVolume;
+   }
+
+   public void SetVolume(float vol)
+   {
+      musicVolume = vol;
+   }
+}
